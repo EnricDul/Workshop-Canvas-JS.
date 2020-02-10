d02371094c2631866b88460b9973b5302974798d
EnricDul <enric.dulieu97@gmail.com>
2020-02-10 08:39:58 +0100

    voilà

---
+++ Introduction/Rectangle/script.js
@@ -0,0 +1,11 @@
+//On sélectionne notre Canvas
+const canvas = document.getElementById("canvas");
+
+//On définit son 'contexte'. Il existe deux types de contextes : 2d et webgl (en expérimentation)
+//C'est à ce contexte que l'on va appliquer nos propriétés.
+const ctx = canvas.getContext("2d");
+
+//VOTRE CODE :
+
+
+
